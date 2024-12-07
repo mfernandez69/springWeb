db9179f7a2dec720b960f16a7f823aad76c94afd
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2024-12-07 14:38:57 +0100

    funcionalidad añadir temporalmente producto

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.example.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,5 +27,22 @@ public class ProductController {
         List<Product> products = this.repository.findAll();
         model.addAttribute("products", products);
         return "product-list";
+    }
+    /*
+    GET http://localhost:8080/products/new
+     */
+    @GetMapping("/new")
+    public String getForm(Model model){
+        model.addAttribute("product", new Product());
+        return "product-form";
+    }
+
+    /*
+    POST http://localhost:8080/products
+     */
+    @PostMapping("/new")
+    public String save(@ModelAttribute("product") Product product){
+        this.repository.save(product);
+        return "redirect:/products";
     }
 }
